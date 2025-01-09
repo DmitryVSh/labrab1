582e40c8e302d4a878fff96eafb8db2c147e00c3
DmitryVSh <dsapov502@gmail.com>
2025-01-09 23:07:02 +0300

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,10 @@
+
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# индекс середины
+middle_index = len(list_players) // 2
+first = list_players[:middle_index] 
+second = list_players[middle_index:]
+
+print(first)
+print(second)
